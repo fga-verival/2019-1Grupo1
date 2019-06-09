6ce4519c8f679cd83cc673722b88a5d7757043a1
Guilherme-Aguiar <gui9627oli@gmail.com>
2019-06-09 18:50:16 -0300

    first case test

--- tbl2_fase3/app/funcoes_transacionais/tests.py
+++ tbl2_fase3/app/funcoes_transacionais/tests.py
@@ -2,20 +2,15 @@ from django.test import TestCase
 from django.test import Client
 from funcoes_transacionais.models import FuncaoTransacional
 
-class Setup_Class(TestCase):
-
-    def setUp(self):
-        self.testando = FuncaoTransicional.objects.create(nome='nome', contaddor='contador', tipo_funcao='EE', complexidade='baixa', qtd_alr=1, qtd_der=1, pontos_de_funcao=2)
-
 class User_Form_Test(TestCase):
 
     # Valid Form Data
     def test_UserForm_valid(self):
-        form = UserForm(data={'email': "user@mp.com", 'password': "user", 'first_name': "user", 'phone': 12345678})
-        self.assertTrue(form.is_valid())
+        form = FuncaoTransacional.objects.create(nome='eaeeeeeeee', contador='contador', tipo_funcao='EE', complexidade='baixa', qtd_alr=1, qtd_der=1, pontos_de_funcao=2)
+        self.assertFalse(form.isvalid())
 
     # Invalid Form Data
     def test_UserForm_invalid(self):
-        form = UserForm(data={'email': "", 'password': "mp", 'first_name': "mp", 'phone': ""})
-        self.assertFalse(form.is_valid())
+        form = FuncaoTransacional.objects.create(nome='', contador='', tipo_funcao='', complexidade='', qtd_alr=0, qtd_der=0, pontos_de_funcao=0)
+        self.assertFalse(form.isvalid())
 
