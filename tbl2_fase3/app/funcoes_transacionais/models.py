1e2b422a9a00d68a197739c73eb59cc14584aa96
bumbleblo <bumbleblo2013@gmail.com>
2019-06-09 01:53:22 -0300

    Adding model
    Signed-off-by: bumbleblo <bumbleblo2013@gmail.com>

--- tbl2_fase3/app/funcoes_transacionais/models.py
+++ tbl2_fase3/app/funcoes_transacionais/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+
+class FuncaoTransacional(models.Model):
+
+    functions_types = (
+        ('EE', 'EE'),
+        ('CE', 'CE'),
+        ('SE', 'SE')
+    )
+
+    complexity_types = (
+        ('baixa', 'baixa'),
+        ('media', 'media'),
+        ('alta', 'alta')
+    )
+      
+    nome = models.CharField(max_length=150)
+
+    contador = models.CharField(max_length=150)
+
+    tipo_funcao = models.CharField(
+        max_length=10,
+        choices=functions_types
+    )
+
+    complexidade = models.CharField(
+        max_length=40,
+        choices=complexity_types
+    )
+
+    qtd_alr = models.IntegerField()
+
+    qtd_der = models.IntegerField()
+    
+    pontos_de_funcao = models.IntegerField()
+
+    data_cadastro = models.DateField()
